7c0b9389c562b4072e239898a76fb40c29a13ab4
Arash Nozarinejad <nozarinejad.arash@gmail.com>
2025-01-06 15:37:53 +0400

    auth routes for login, register, and /me created

---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { AuthRequest } from "@/types/auth.types";
+import { registerUser, loginUser } from "@/controllers/auth.controller";
+import { authenticate } from "@/middleware/auth.middleware";
+
+const router = Router();
+
+router.post('/login', loginUser);
+router.post('/register', registerUser);
+router.get('/me', authenticate, (req: AuthRequest, res) => {
+    res.json({ user: req.user })
+});
+
+export default router;
