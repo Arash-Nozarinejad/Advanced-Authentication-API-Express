c85c6e213f39073ac3ec5ab6b7d5310f3fa9a0fa
Arash Nozarinejad <nozarinejad.arash@gmail.com>
2025-01-07 12:08:50 +0400

    validation added to the auth routes

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { AuthRequest } from "../types/auth.types";
 import { registerUser, loginUser } from "../controllers/auth.controller";
 import { authenticate } from "../middleware/auth.middleware";
+import { validate } from "../middleware/validation.middleware";
+import { registerValidation, loginValidation } from "@/validation/auth.validation";
 
 const router = Router();
 
-router.post('/login', loginUser);
-router.post('/register', registerUser);
+router.post('/login', validate(loginValidation), loginUser);
+router.post('/register', validate(registerValidation),registerUser);
 router.get('/me', authenticate, (req: AuthRequest, res) => {
     res.json({ user: req.user })
 });
