68a23f0ee6e3a23cef4444176d14b2cc4ba0cdff
Arash Nozarinejad <nozarinejad.arash@gmail.com>
2025-01-07 14:08:56 +0400

    login attempt limiter added to /login route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -4,11 +4,12 @@ import { registerUser, loginUser } from "../controllers/auth.controller";
 import { authenticate } from "../middleware/auth.middleware";
 import { validate } from "../middleware/validation.middleware";
 import { registerValidation, loginValidation } from "../validation/auth.validation";
+import { authLimiter } from "../middleware/rate-limit.middleware";
 
 const router = Router();
 
 router.post('/login', validate(loginValidation), loginUser);
-router.post('/register', validate(registerValidation),registerUser);
+router.post('/register', authLimiter, validate(registerValidation),registerUser);
 router.get('/me', authenticate, (req: AuthRequest, res) => {
     res.json({ user: req.user })
 });
